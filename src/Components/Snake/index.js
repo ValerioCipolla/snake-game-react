c4d7bfed1d05d3e920519d15ed462f996a694262
valeriocipolla <valeriocipolla92@gmail.com>
2022-01-24 13:43:44 +0000

    make state change correctly in response to keydown

--- src/Components/Snake/index.js
+++ src/Components/Snake/index.js
@@ -2,8 +2,38 @@ import React, { useEffect, useState, useReducer } from "react";
 import SnakePart from "../SnakePart";
 import { nanoid } from "nanoid";
 
+function reducer(state, action) {
+  if (action.type === "change-direction-left") {
+    if (state.direction === "right") {
+      return state;
+    } else {
+      return { ...state, direction: action.payload.direction };
+    }
+  } else if (action.type === "change-direction-up") {
+    if (state.direction === "down") {
+      return state;
+    } else {
+      return { ...state, direction: action.payload.direction };
+    }
+  } else if (action.type === "change-direction-right") {
+    if (state.direction === "left") {
+      return state;
+    } else {
+      return { ...state, direction: action.payload.direction };
+    }
+  } else if (action.type === "change-direction-down") {
+    if (state.direction === "up") {
+      return state;
+    } else {
+      return { ...state, direction: action.payload.direction };
+    }
+  } else {
+    return state;
+  }
+}
+
 const Snake = () => {
-  const [direction, setDirection] = useState("right");
+  const [state, dispatch] = useReducer(reducer, { direction: "right" });
 
   const [snakeArray, setSnakeArray] = useState([
     { top: 0, left: 0 },
@@ -12,19 +42,32 @@ const Snake = () => {
     { top: 0, left: 30 },
   ]);
 
+  function changeDirection(e) {
+    if (e.key === "ArrowLeft") {
+      dispatch({
+        type: "change-direction-left",
+        payload: { direction: "left" },
+      });
+    } else if (e.key === "ArrowUp") {
+      dispatch({
+        type: "change-direction-up",
+        payload: { direction: "up" },
+      });
+    } else if (e.key === "ArrowRight") {
+      dispatch({
+        type: "change-direction-right",
+        payload: { direction: "right" },
+      });
+    } else if (e.key === "ArrowDown") {
+      dispatch({
+        type: "change-direction-down",
+        payload: { direction: "down" },
+      });
+    }
+  }
+
   useEffect(() => {
-    window.addEventListener("keydown", function (e) {
-      console.log(e.key);
-      if (e.key === "ArrowLeft") {
-        setDirection("left");
-      } else if (e.key === "ArrowUp") {
-        setDirection("up");
-      } else if (e.key === "ArrowRight") {
-        setDirection("right");
-      } else if (e.key === "ArrowDown") {
-        setDirection("down");
-      }
-    });
+    window.addEventListener("keydown", (e) => changeDirection(e));
   }, []);
 
   useEffect(() => {
