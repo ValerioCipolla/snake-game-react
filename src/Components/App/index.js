c58efb0d9a90f503503bcab22eebbc5b5b9c77d9
valeriocipolla <valeriocipolla92@gmail.com>
2022-01-24 10:15:22 +0000

    changed state to snake

--- src/Components/App/index.js
+++ src/Components/App/index.js
@@ -3,15 +3,11 @@ import Snake from "../Snake";
 import "./app.css";
 
 const Board = () => {
-  const [snakeArray, setSnakeArray] = useState([
-    { top: 0, left: 0 },
-    { top: 0, left: 10 },
-    { top: 0, left: 20 },
-  ]);
+
 
   return (
     <div id="game-board" className="game-board">
-      <Snake coords={snakeArray} />
+      <Snake/>
     </div>
   );
 };
--- src/Components/Snake/index.js
+++ src/Components/Snake/index.js
@@ -1,11 +1,27 @@
-import React from "react";
+import React, { useState } from "react";
 import SnakePart from "../SnakePart";
 import { nanoid } from "nanoid";
 
-const Snake = ({ coords }) => {
+const Snake = () => {
+  const [snakeArray, setSnakeArray] = useState([
+    { top: 0, left: 0 },
+    { top: 0, left: 10 },
+    { top: 0, left: 20 },
+    { top: 0, left: 30 },
+  ]);
+
+  // setInterval(snakeMove, 1000);
+
+  // function snakeMove() {
+  //   let newSnakeArray = snakeArray.map(function (item) {
+  //     return { top: item.top, left: (item.left += 10) };
+  //   });
+  //   setSnakeArray(newSnakeArray);
+  // }
+
   return (
     <div>
-      {coords.map((coord) => (
+      {snakeArray.map((coord) => (
         <SnakePart key={nanoid()} coords={coord} />
       ))}
     </div>
